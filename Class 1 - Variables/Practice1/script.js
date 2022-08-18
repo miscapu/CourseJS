9ae721058120ad06805a017678be9808a08a71dc
miscapu <mignan2010@outlook.com>
2022-08-17 22:56:12 -0300

    Class 1 - Variables: Declared and Print in display

---
+++ Class 1 - Variables/Practice1/script.js
@@ -0,0 +1,16 @@
+/**
+ * =========================
+ * Declare Variables
+ * ========================
+ */
+var x   =   3;
+var y   =   9;
+var z   =   y - x;
+
+/**
+ * =========================
+ * Print variables to display
+ * into element demo id
+ * =========================
+ */
+document.getElementById( "demo" ).innerHTML =   "The value to z is: " + z;
---
+++ Class 1 - Variables/script.js
@@ -0,0 +1,15 @@
+/*
+    =========================
+    Declare Variable
+    =========================
+ */
+var x   =   3;
+var y   =   4;
+var z   =   x + y;
+
+/*
+    ==========================
+    print variable
+    ===========================
+ */
+document.getElementById( "demo" ).innerHTML =   "The value of z is: " + z;
